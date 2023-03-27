80a38c411ca5c05f0da6ec87ea1e32ff6be70339
Aslam Miya <64162091+aslammiya@users.noreply.github.com>
2023-03-27 22:02:14 +0530

    finish

--- wetaher/settings.py
+++ wetaher/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-k#$#)ss-!^bkvd*el(!vex)8$ysyrcy=097%7+r&&%op3j1!#-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
